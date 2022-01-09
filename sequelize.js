8a70212f46ac2a6edda9c19ec0b6775018fa03b1
yoni <yontand@gmail.com>
2022-01-09 22:32:17 +0200

    add env to PostgresSQL

--- sequelize.js
+++ sequelize.js
@@ -1,16 +1,35 @@
 import Sequelize from "sequelize";
+import dot from 'dotenv';
+dot.config()
 
-const seq = new Sequelize("works", "root", "123456", {
-  host: "localhost",
+ const uriDb = "postgres://sxeogbfumoppas:51a05111450064513c5d85eff87b187ab429adc34b5209c635ea86bdc256e63c@ec2-44-198-214-172.compute-1.amazonaws.com:5432/dcpikb1kei60pu";
+// const seq = new Sequelize("works", "root", "123456", {
+//   host: "localhost",
 
-  dialect: "mysql",
-  pool: {
-    max: 5,
-    min: 0,
-    acquire: 30000,
-    idle: 10000,
-  },
-});
+//   dialect: "mysql",
+//   pool: {
+//     max: 5,
+//     min: 0,
+//     acquire: 30000,
+//     idle: 10000,
+//   },
+// });
 
-
-export default seq;
+const seqUri = new Sequelize(process.env.DB_URI,{
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false
+    }
+  }
+}
+)
+seqUri
+  .authenticate()
+  .then(() => {
+    console.log('Connection has been established successfully.');
+  })
+  .catch(err => {
+    console.error('Unable to connect to the database:', err);
+  });
+export default seqUri;
